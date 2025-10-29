8a73c5c5056b4d6d1b77e079a4bb462d2b2308ec
Ksenia Mukhamedzyanova <ksenia.mukhamedzyanova@mail.ru>
2025-10-29 21:02:29 +0500

    allure framework added

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,17 +1,22 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
+import allure
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Введите логин'
 EMPTY_PASSWORD_ERROR = 'Введите пароль'
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при пустой форме авторизации")
 def test_empty_login_and_password_fields(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.suite("Проверка формы авторизации")
+@allure.title("Проверка ошибки при незаполненном поле 'Пароль'")
 def test_empty_password_field(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
